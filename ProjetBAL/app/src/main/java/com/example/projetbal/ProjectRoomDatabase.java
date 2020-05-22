08628a5111b950aff270aa9f0de57e8455a2f9a0
GregPhi <gregoire.philippot@laposte.net>
2020-05-22 11:37:57 +0200

    pji

--- ProjetBAL/app/src/main/java/com/example/projetbal/ProjectRoomDatabase.java
+++ ProjetBAL/app/src/main/java/com/example/projetbal/ProjectRoomDatabase.java
@@ -3,10 +3,12 @@ package com.example.projetbal;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import com.example.projetbal.dao.FoundLivreDao;
 import com.example.projetbal.dao.LivreDao;
-import com.example.projetbal.object.EtatsLivre;
-import com.example.projetbal.object.Livre;
-import com.example.projetbal.object.StatutsLivre;
+import com.example.projetbal.object.FoundLivre;
+import com.example.projetbal.object.book.EtatsLivre;
+import com.example.projetbal.object.book.Livre;
+import com.example.projetbal.object.book.StatutsLivre;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
@@ -14,9 +16,10 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {Livre.class},version = 1)
+@Database(entities = {Livre.class, FoundLivre.class},version = 4)
 public abstract class ProjectRoomDatabase extends RoomDatabase {
     public abstract LivreDao bookDao();
+    public abstract FoundLivreDao foundLivreDao();
 
     private static volatile ProjectRoomDatabase INSTANCE;
 
@@ -48,27 +51,34 @@ public abstract class ProjectRoomDatabase extends RoomDatabase {
 
     private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
 
-        private final LivreDao mDao;
+        private final LivreDao mBookDao;
+        private final FoundLivreDao mFoundBookDao;
 
         PopulateDbAsync(ProjectRoomDatabase db) {
-            mDao = db.bookDao();
+            mBookDao = db.bookDao();
+            mFoundBookDao = db.foundLivreDao();
         }
 
         @Override
         protected Void doInBackground(final Void... params) {
-            mDao.deleteAll();
+            mBookDao.deleteAll();
+            mFoundBookDao.deleteAll();
             Livre livre1 = new Livre();
             livre1.setCode_barre("9782013235327");
             livre1.setTitle("Hobbit");
             livre1.setEtats(EtatsLivre.NEUF.toString());
-            livre1.setStatuts(StatutsLivre.PREPARE.toString());
+            livre1.setStatuts(StatutsLivre.PREPARE.getType());
             Livre livre2 = new Livre();
             livre2.setCode_barre("9782290019436");
             livre2.setTitle("GOT");
             livre2.setEtats(EtatsLivre.BONETAT.toString());
-            livre2.setStatuts(StatutsLivre.PREPARE.toString());
-            mDao.insert(livre1);
-            mDao.insert(livre2);
+            livre2.setStatuts(StatutsLivre.PREPARE.getType());
+            mBookDao.insert(livre1);
+            mBookDao.insert(livre2);
+            FoundLivre f1 = new FoundLivre(livre1);
+            FoundLivre f2 = new FoundLivre(livre2);
+            mFoundBookDao.insert(f1);
+            mFoundBookDao.insert(f2);
             return null;
         }
     }
